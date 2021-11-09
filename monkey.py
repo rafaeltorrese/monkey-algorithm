5060fb0565cdb5d5f9cb64d2c7dcba7ca6639b63
Rafael Torres Escobar <torresc.rafael@gmail.com>
2021-11-08 20:37:19 -0600

    Function names were changed

--- monkey.py
+++ monkey.py
@@ -8,7 +8,7 @@ from functions import initialize
 M = 10
 #%%
 if __name__ == "__main__":
-    monkeys = initialize.initialize_positions(3)
+    monkeys = initialize.positions(3)
     v = initialize.deltas(monkeys, a=0.0001)
     print(v)
     print(monkeys[0])
@@ -23,6 +23,6 @@ if __name__ == "__main__":
     f2 = initialize.objective(monkeys[0] - v[0])
     print(f1 - f2)
     print((f1 - f2) / 2 * v[0] )
-    # print(monkeys)
+    print(monkeys)
 
 # %%
